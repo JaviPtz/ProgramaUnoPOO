b4b8967d489e403205399c303ee54d98068b1f3c
JaviPtz <jam.pastaz@yavirac.deu.ec>
2020-06-26 13:39:52 -0500

    primer commit

---
+++ CosasUsanPersonas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosasUsanPersonas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
